025d7ddb60912589ac7646b2fdcc173d4dabef0a
massu <m.assum@aol.de>
2023-09-29 18:45:04 +0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World!
+          (This is a Test)
         </p>
         <a
           className="App-link"
